1d5577cb04b6013b0fb0c8915312c9cfc60442c3
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2023-04-08 12:49:27 +0000

    add Create and List commands

--- main.go
+++ main.go
@@ -3,57 +3,43 @@ package main
 import (
 	"flag"
 	"fmt"
-	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// Reminder represents a reminder with a task description, due date/time, and tags.
-type Reminder struct {
-	Description string
-	DueTime     time.Time
-	Tags        []string
-}
 
 func main(){
-	description := flag.String("description", "", "Task description")
-	dueTime := flag.String("due", "", "Due date/time (YYYY-MM-DD HH:MM)")
-	tags := flag.String("tags", "", "Tags separated by commas")
+	createCmd := flag.NewFlagSet("create", flag.ExitOnError)
+	listCmd := flag.NewFlagSet("list", flag.ExitOnError)
+
+	description := createCmd.String("description", "", "Task description")
+	dueTime := createCmd.String("due", "", "Due date/time (YYYY-MM-DD HH:MM)")
+	tags := createCmd.String("tags", "", "Tags separated by commas")
 
 	flag.Parse()
 
-	if *description == "" {
-		fmt.Println("Error: Task description is required")
+	// Check if subcommand "create" is provided
+	if flag.NArg() > 0 && flag.Arg(0) == "create" {
+		createCmd.Parse(flag.Args()[1:])
+		reminder, err := Create(*description, *dueTime, *tags)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// print print reminder
+		fmt.Println("Reminder created successfully:")
+		fmt.Println("Description:", reminder.Description)
+		fmt.Println("Due date/time:", reminder.DueTime)
+		fmt.Println("Tags:", reminder.Tags)
+	} else if flag.NArg() > 0 && flag.Arg(0) == "list" {
+		listCmd.Parse(flag.Args()[1:])
+		reminders := List()
+
+		fmt.Println(reminders)
+	} else {
+		fmt.Println(flag.Arg(0), "is not a reminderBot command.")
 		flag.PrintDefaults()
 		return
 	}
-
-	if *dueTime == "" {
-		fmt.Println("Error: Due date/time is required.")
-		flag.PrintDefaults()
-		return
-	}
-
-	due, err := time.Parse("2006-01-02 15:04", *dueTime)
-	if err != nil {
-		fmt.Println("Error: Invalid due date/time format. Please use YYYY-MM-DD HH:MM.")
-		return
-	}
-
-	// Parse tags
-	tagsArr := []string{}
-	if *tags != "" {
-		tagsArr = append(tagsArr, *tags)
-	}
-
-	reminder := Reminder{
-		Description: *description,
-		DueTime: due,
-		Tags: tagsArr,
-	}
-
-	fmt.Println("Reminder created successfully:")
-	fmt.Println("Description:", reminder.Description)
-	fmt.Println("DueTime:", reminder.DueTime)
-	fmt.Println("Tags:", reminder.Tags)
 }
