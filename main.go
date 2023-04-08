58cadb618448fc3b659ae43622d903bf17821e01
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2023-04-08 12:57:46 +0000

    skeleton command router

--- main.go
+++ main.go
@@ -9,16 +9,19 @@ import (
 
 
 func main(){
+	// Define "create" and "list" subcommands
 	createCmd := flag.NewFlagSet("create", flag.ExitOnError)
 	listCmd := flag.NewFlagSet("list", flag.ExitOnError)
 
+	// define create subcommand flagsß
 	description := createCmd.String("description", "", "Task description")
 	dueTime := createCmd.String("due", "", "Due date/time (YYYY-MM-DD HH:MM)")
 	tags := createCmd.String("tags", "", "Tags separated by commas")
 
+	// Parse command-line arguments
 	flag.Parse()
 
-	// Check if subcommand "create" is provided
+	// Check which subcommand is provided
 	if flag.NArg() > 0 && flag.Arg(0) == "create" {
 		createCmd.Parse(flag.Args()[1:])
 		reminder, err := Create(*description, *dueTime, *tags)
@@ -37,6 +40,24 @@ func main(){
 		reminders := List()
 
 		fmt.Println(reminders)
+	} else if flag.NArg() > 0 && flag.Arg(0) == "delete" {
+		fmt.Println("reminderBot", flag.Args()[0], "is not implemented yet")
+		return
+	} else if flag.NArg() > 0 && flag.Arg(0) == "edit" {
+		fmt.Println("reminderBot", flag.Args()[0], "is not implemented yet")
+		return
+	} else if flag.NArg() > 0 && flag.Arg(0) == "snooze" {
+		fmt.Println("reminderBot", flag.Args()[0], "is not implemented yet")
+		return
+	} else if flag.NArg() > 0 && flag.Arg(0) == "search" {
+		fmt.Println("reminderBot", flag.Args()[0], "is not implemented yet")
+		return
+	} else if flag.NArg() > 0 && flag.Arg(0) == "config" {
+		fmt.Println("reminderBot", flag.Args()[0], "is not implemented yet")
+		return
+	} else if flag.NArg() > 0 && flag.Arg(0) == "help" {
+		fmt.Println("reminderBot", flag.Args()[0], "is not implemented yet")
+		return
 	} else {
 		fmt.Println(flag.Arg(0), "is not a reminderBot command.")
 		flag.PrintDefaults()
