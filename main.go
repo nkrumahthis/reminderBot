0f694ad34f40e9e69f2506181781c296cb1071c1
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2023-04-09 19:06:00 +0000

    parse create command

--- main.go
+++ main.go
@@ -5,36 +5,20 @@ import (
 	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
+	commands "github.com/nkrumahthis/reminderBot/commands"
 )
 
 
 func main(){
 	// Define "create" and "list" subcommands
-	createCmd := flag.NewFlagSet("create", flag.ExitOnError)
 	listCmd := flag.NewFlagSet("list", flag.ExitOnError)
-
-	// define create subcommand flagsß
-	description := createCmd.String("description", "", "Task description")
-	dueTime := createCmd.String("due", "", "Due date/time (YYYY-MM-DD HH:MM)")
-	tags := createCmd.String("tags", "", "Tags separated by commas")
 
 	// Parse command-line arguments
 	flag.Parse()
 
 	// Check which subcommand is provided
 	if flag.NArg() > 0 && flag.Arg(0) == "create" {
-		createCmd.Parse(flag.Args()[1:])
-		reminder, err := Create(*description, *dueTime, *tags)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		// print print reminder
-		fmt.Println("Reminder created successfully:")
-		fmt.Println("Description:", reminder.Description)
-		fmt.Println("Due date/time:", reminder.DueTime)
-		fmt.Println("Tags:", reminder.Tags)
+		commands.ParseCreateCmd()
 	} else if flag.NArg() > 0 && flag.Arg(0) == "list" {
 		listCmd.Parse(flag.Args()[1:])
 		reminders := List()
