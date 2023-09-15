ea8564c3ae8609ff41d61be0b57a7da7eb331df8
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2023-09-15 11:39:34 +0000

    clean database initialization

--- db/initialize.go
+++ db/initialize.go
@@ -1,16 +1,12 @@
 package db
 
 import (
-	"fmt"
 	"os"
 )
 
 func Init(){
 	DB_PATH := "./data/db.db"
-
-	fmt.Println(DB_PATH)
 	_, err := os.Stat(DB_PATH)
-	fmt.Println(err)
 	if os.IsNotExist(err){
 		file, err := os.Create(DB_PATH)
 		if err != nil {
