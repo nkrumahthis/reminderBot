34023e9788dba7c4c4d485c40b37a3140bb7ba37
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-04-01 21:57:37 +0000

    move old cli

---
+++ old-main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/nkrumahthis/reminderBot/commands"
+	"github.com/nkrumahthis/reminderBot/db"
+)
+
+
+func oldmain(){
+	db.Init()
+
+	// Parse command-line arguments
+	flag.Parse()
+
+	// Check which subcommand is provided
+	if flag.NArg() > 0 && flag.Arg(0) == "create" {
+		commands.ParseCreateCmd()
+		return
+	} else if flag.NArg() > 0 && flag.Arg(0) == "list" {
+		commands.ParseListCmd()
+		return
+	} else if flag.NArg() > 0 && flag.Arg(0) == "delete" {
+		commands.ParseDeleteCmd()
+		return
+	} else if flag.NArg() > 0 && flag.Arg(0) == "edit" {
+		fmt.Println("reminderBot Edit is not implemented yet")
+		return
+	} else if flag.NArg() > 0 && flag.Arg(0) == "snooze" {
+		fmt.Println("reminderBot", flag.Args()[0], "is not implemented yet")
+		return
+	} else if flag.NArg() > 0 && flag.Arg(0) == "search" {
+		fmt.Println("reminderBot", flag.Args()[0], "is not implemented yet")
+		return
+	} else if flag.NArg() > 0 && flag.Arg(0) == "config" {
+		fmt.Println("reminderBot", flag.Args()[0], "is not implemented yet")
+		return
+	} else if flag.NArg() > 0 && flag.Arg(0) == "help" {
+		fmt.Println("reminderBot", flag.Args()[0], "is not implemented yet")
+		return
+	} else {
+		fmt.Println(flag.Arg(0), "is not a reminderBot command.")
+		flag.PrintDefaults()
+		return
+	}
+}
